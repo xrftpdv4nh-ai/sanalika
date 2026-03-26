0d147d1fd5ac4ceec7a4a7355e301157ac729e9a
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-26 02:26:23 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,4 @@
 <?php
 
 $CONFIG = [];
-
-$CONFIG['site_name'] = 'Marhab Modern';
-$CONFIG['site_url'] = getenv('APP_URL') ?: 'http://localhost:8000';
-$CONFIG['admin_email'] = getenv('ADMIN_EMAIL') ?: 'admin@example.com';
+$CONFIG['site_name'] = 'Marhab Test';
