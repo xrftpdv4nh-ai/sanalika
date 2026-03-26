13156a4c53e870e9e7e3f3d7a647d7f93aa4c2a7
xrftpdv4nh-ai <xrftpdv4nh@privaterelay.appleid.com>
2026-03-26 02:02:25 +0200

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$CONFIG = [];
+
+$CONFIG['site_name'] = 'Marhab Modern';
+$CONFIG['site_url'] = getenv('APP_URL') ?: 'http://localhost:8000';
+$CONFIG['admin_email'] = getenv('ADMIN_EMAIL') ?: 'admin@example.com';
